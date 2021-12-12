cf3fef6b6deadbc0524f146cb05e992367f6f1df
ChiaraCatenaccio <91664062+ChiaraCatenaccio@users.noreply.github.com>
2021-12-12 01:16:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,15 @@ int main() {
     int pos;
     char a[10][20], f[1][20];
     for (int i=0; i<10; i++){
-        cin > >a[i];
+        cin >> a[i];
     }
-    cin>>f[0];
+    cin >> f[0];
 
     if (ricerca(a, f, pos)){
         cout << pos << endl;
+    }else{
+        cout << "non presente" << endl;
     }
+    
     return 0;
 }
